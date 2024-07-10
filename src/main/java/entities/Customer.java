9a971f27e69c61bf369d21b1a324532e637c0abb
Nese <neseilhannn@gmail.com>
2024-07-10 19:00:43 +0300

    files

---
+++ src/main/java/entities/Customer.java
@@ -0,0 +1,11 @@
+package entities;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "customer_id", length = )
+}
